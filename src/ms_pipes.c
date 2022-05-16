735b15435f86a80c25da03e4eb4d47833e8d96c0
Eneko Camara <ecamara@c3r8s6.42urduliz.com>
2022-05-16 11:42:41 +0200

    16 may 11:40

--- src/ms_pipes.c
+++ src/ms_pipes.c
@@ -6,7 +6,7 @@
 /*   By: ecamara <ecamara@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 11:51:58 by ecamara           #+#    #+#             */
-/*   Updated: 2022/05/13 11:54:06 by ecamara          ###   ########.fr       */
+/*   Updated: 2022/05/16 11:37:14 by ecamara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ void	ft_start_pipes(t_data *data)
 	write(2,"start\n",6);
 	dup2(data->fd[1][1], STDOUT_FILENO);
 	close(data->fd[0][1]);
-	close(data->fd[1][0]);
-	close(data->fd[0][0]);
+	//close(data->fd[1][0]);
+	//close(data->fd[0][0]);
 	close(data->fd[1][1]);
 	write(2,"start\n",6);
 	//pipe(data->fd[0]);
@@ -39,7 +39,9 @@ void	ft_mid_pipes(t_data *data)
 {
 	(void)data;
 	write(2,"mid\n",4);
-	data->fd[0][1] = data->fd[1][0];
+	close(data->fd[1][1]);
+	close(data->fd[0][1]);
+	data->fd[0][0] = data->fd[1][0];
 	pipe(data->fd[1]);
 	dup2(data->fd[0][0], STDIN_FILENO);
 	dup2(data->fd[1][1], STDOUT_FILENO);
@@ -60,10 +62,10 @@ void	ft_end_pipes(t_data *data)
 	write(2,"end\n",4);
 	//ft_print_fd(data->fd[1][0]);
 	dup2(2, STDOUT_FILENO);
-	/*close(data->fd[0][1]);
+	close(data->fd[0][1]);
 	close(data->fd[1][0]);
 	close(data->fd[0][0]);
-	close(data->fd[1][1]);*/
+	close(data->fd[1][1]);
 	write(2,"end\n",4);
 }
 
