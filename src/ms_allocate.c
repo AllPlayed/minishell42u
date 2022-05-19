123a37ca53d599c60119c78a2a30d7e0f8f2b39c
Eneko Camara <eneko.ca.pal@gmail.com>
2022-05-19 12:54:49 +0200

    19 may 12:54

--- src/ms_allocate.c
+++ src/ms_allocate.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_allocate.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ullorent <ullorent@student.42urduliz.co    +#+  +:+       +#+        */
+/*   By: ecamara <ecamara@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 13:55:09 by ecamara           #+#    #+#             */
-/*   Updated: 2022/05/12 17:34:27 by ullorent         ###   ########.fr       */
+/*   Updated: 2022/05/19 12:48:51 by ecamara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 void	ft_allocate(t_data *data, int infile, int outfile, int cmd)
 {
-	//printf("CMD = %d INFILE = %d OUTFILE = %d\n", cmd, infile, outfile);
+	printf("CMD = %d INFILE = %d OUTFILE = %d\n", cmd, infile, outfile);
 	data->infile.files = malloc((infile + 1) * sizeof(char *));
 	data->infile.modes = malloc((infile) * sizeof(int));
 	data->infile.files[infile] = NULL;
 	data->outfile.files = malloc((outfile + 1) * sizeof(char *));
 	data->outfile.modes = malloc((outfile) * sizeof(int));
 	data->outfile.files[outfile] = NULL;
+	printf("cmd = %d\n", cmd);
 	data->cmd = malloc((cmd + 1) * sizeof(char *));
 	data->cmd[cmd] = NULL;
+	data->cmd_n = cmd;
+	data->outfile_n = outfile;
+	data->infile_n = infile;
 }
