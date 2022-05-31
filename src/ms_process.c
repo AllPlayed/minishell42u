35b888d12d2dc6c8c0dca0423495ac4ae0842035
Eneko Camara <eneko.ca.pal@gmail.com>
2022-05-31 12:53:52 +0200

    31 may 12:53

--- src/ms_process.c
+++ src/ms_process.c
@@ -6,7 +6,7 @@
 /*   By: ecamara <ecamara@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 12:39:14 by ecamara           #+#    #+#             */
-/*   Updated: 2022/05/30 14:51:21 by ecamara          ###   ########.fr       */
+/*   Updated: 2022/05/31 12:52:28 by ecamara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,6 +130,40 @@ char	**ft_get_path(t_data *data)
 	return(ft_split(data->env[index], ':'));
 }
 
+int	quit_path(t_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (data->cmd[0][i])
+	{
+		if (data->cmd[0][i] == '/')
+			j = i;
+		i++;
+	}
+	if (j == 0)
+		return (1);
+	data->cmd[0] = data->cmd[0] + j + 1;
+	return (0);
+}
+
+void	search_cmd2(t_data *data)
+{
+	char	*temp;
+
+	if (access(data->cmd[0], X_OK) == 0)
+	{
+		temp = ft_substr(data->cmd[0], 0, ft_strlen(data->cmd[0]));
+		data->cmd[data->cmd_n] = NULL;//arreglar luego ls  | wc 0 0 0
+		ft_close_pipes(data);
+		if (quit_path(data))
+			return ;
+		execve(temp, data->cmd, data->env);
+		exit(0);
+	}
+}
+
 void	ft_search_cmd(t_data *data)
 {
 	char	*temp;
@@ -140,6 +174,7 @@ void	ft_search_cmd(t_data *data)
 	path = ft_get_path(data);
 	if (path == NULL)
 		exit(0);
+	search_cmd2(data);
 	while (data->path[i] != NULL && data->cmd[0][0] != '\0')
 	{
 		temp = ft_strjoin(data->path[i], "/");
